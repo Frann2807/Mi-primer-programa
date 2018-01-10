293f3d8334fb34eca344eb9863b4b3b62341fb86
Fran Miranda <francisconehuenmiranda>
2018-01-10 17:23:33 -0300

    Añadiendo "el_mayor_entre_tres"

---
+++ el_mayor_entre_tres.py
@@ -0,0 +1,9 @@
+primer_numero = int(input("Primer Numero: "))
+
+segundo_numero = int(input("Segundo Numero: "))
+
+tercer_numero = int(input("Tercer Numero: "))
+
+resultado = max(primer_numero, segundo_numero, tercer_numero)
+
+print("El numero mas grande es: {}".format(resultado))
