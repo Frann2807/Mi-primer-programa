4fb47d320b6fdb9eb57480aa05aa996fcf96f2cf
Fran Miranda <francisconehuenmiranda>
2018-01-10 18:17:28 -0300

    Modificando pequeños detalles "challenge_acepted"

--- challenge_acepted.py
+++ challenge_acepted.py
@@ -1,18 +1,18 @@
 
-number_to_guess = 7
+number_to_guess = 5
 
 user_number = int(input("Tienes 5 intentos para adivianar el numero entre el 1 y el 10.Adivina el numero: "))
 
 if user_number == number_to_guess:
-    print("Has ganado")
+    print("WOW! Has ganado")
 
 else:
-    print("Has perdido.")
+    print("Mala suerte! Pero tranquilo, Solo es el primer intento.")
     user_number = int(input("Intentalo de nuevo: "))
     if user_number == number_to_guess:
         print("Lo has logrado!")
     else:
-        print("Mala suerte")
+        print("Mala suerte!")
         user_number = int(input("Puedes hacerlo, Intentalo de nuevo!: "))
         if user_number == number_to_guess:
             print("Te dije que podias hacerlo, Felicidades!")
