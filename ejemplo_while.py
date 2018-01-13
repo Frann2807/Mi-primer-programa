4dcaf96743b9bc94eda9b842aecce3bbe2002f1e
Fran Miranda <francisconehuenmiranda>
2018-01-13 16:10:12 -0300

    Agregando archivo "ejemplo_while"

---
+++ ejemplo_while.py
@@ -0,0 +1,18 @@
+numero_inicial = 10
+
+print(numero_inicial)
+
+if numero_inicial % 2 == 0:
+    print("Este numero es el incial y par")
+else:
+    print("Este es el numero inicial y es impar")
+
+while numero_inicial > 0:
+    numero_inicial -= 1
+    print(numero_inicial)
+    if numero_inicial % 2 == 0:
+        print("Este numero es par")
+    else:
+        print("Este numero es impar")
+
+print("He terminado")
