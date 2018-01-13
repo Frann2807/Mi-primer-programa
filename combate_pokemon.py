b8b7bc65090f8b98068fa891f3583f1b77f15460
Fran Miranda <francisconehuenmiranda>
2018-01-13 19:06:51 -0300

    Corrigiendo "combate_pokemon"

---
+++ combate_pokemon.py
@@ -0,0 +1,43 @@
+pokemon_elegido = input("¿Contra que pokemon quieres luchar?(Squirtle / Charmander / Bulbasaur): ").upper()
+
+vida_pikachu = 100
+vida_enemigo = 0
+ataque_pokemon = 0
+
+# Elegimos un Pokemon
+if pokemon_elegido == "SQUIRTLE":
+    vida_enemigo = 90
+    nombre_pokemon = "Squirtle"
+    ataque_pokemon = 8
+
+elif pokemon_elegido == "CHARMANDER":
+    vida_enemigo = 80
+    nombre_pokemon = "Charmander"
+    ataque_pokemon = 7
+
+elif pokemon_elegido == "BULBASAUR":
+    vida_enemigo = 100
+    nombre_pokemon = "Bulbasaur"
+    ataque_pokemon = 10
+
+while vida_pikachu > 0 and vida_enemigo > 0:
+    ataque_elegido = input("¿Que ataque deseas utilizar? (Chispazo / Bola voltio) :").upper()
+
+    if ataque_elegido == "CHISPAZO":
+        vida_enemigo -= 10
+
+    elif ataque_elegido == "BOLA VOLTIO":
+        vida_enemigo -= 12
+
+    print("La vida del {} ahora es {}".format(nombre_pokemon, vida_enemigo))
+
+    print("{} te hace un ataque de {} daño".format(nombre_pokemon, ataque_pokemon))
+    vida_pikachu -= ataque_pokemon
+
+if vida_enemigo <= 0:
+    print("Has ganado")
+
+if vida_pikachu <= 0:
+    print("Has perdido")
+
+print("El combate ha terminado")
