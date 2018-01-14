741673cc46c7a4d6d5b2a305773da79206a96af3
Fran Miranda <francisconehuenmiranda>
2018-01-13 22:18:43 -0300

    Añadiendo "homework_celsius_to_fahrenheit"

---
+++ homework_celsius_to_fahrenheit.py
@@ -0,0 +1,7 @@
+print("Este programa te facilita la conversion de Grados Celcius a Grados Fahrenheit")
+
+grados_celcius = int(input("Dime el valor de los grados Celcius: "))
+
+grados_fahrenheit = ((grados_celcius * 9/5) + 32)
+
+print("{} grados Celsius equivalen a {} grados Fahrenheit.".format(grados_celcius, grados_fahrenheit))
