efa1eef7fb44c491ab97fc9801c1aaa9f92ee424
Frann <francisconehuenmiranda@gmail.com>
2018-01-22 16:37:03 -0300

    Subiendo "conceptos_practica" Parte 2

--- conceptos_practica/parte_dos/lista_de_tipos/ejercicio_tres_forma_dos.py
+++ conceptos_practica/parte_dos/lista_de_tipos/ejercicio_tres_forma_dos.py
@@ -1,11 +1,17 @@
+"""
+Dada una lista mixta de enteros y strings, devolver dos listas, una con todos los enteros y otra con todas las strings.
+"""
+
 lista_datos = ["asd", 1, "abc", 3, 7, "Fran"]
 lista_num = []
 lista_str = []
 
 for dato in lista_datos:
-    if dato.isalpha():
+
+    if type(dato) == str:
         lista_str.append(dato)
-    elif dato.isdigit():
+
+    elif type(dato) == int:
         lista_num.append(dato)
 
 print("Lista de numeros: {}".format(lista_num))
